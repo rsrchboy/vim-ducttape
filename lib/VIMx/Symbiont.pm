9028b3f4abc7070e9ff99f883bd157a4d3c6c435
Chris Weyl <cweyl@alumni.drew.edu>
2017-11-28 19:51:28 -0600

    Simplify our generated VimL for stub functions
    90% of what was going on in the generated viml was Perl, anyways.  May
    as well make life a touch easier and just handle that out here.

--- lib/VIMx/Symbiont.pm
+++ lib/VIMx/Symbiont.pm
@@ -30,30 +30,30 @@ sub _class_to_vim_ns { (my $ns = shift) =~ s/::/#/g; $ns }
 sub function {
     my ($name, $coderef) = @_;
 
-    my ($pkg) = caller;
-
     # TODO just return if we're not inside vim
 
+    my ($pkg) = caller;
     my $perl_name = "${pkg}::${name}";
     my $vim_ns    = _class_to_vim_ns($pkg);
     $name = _class_to_vim_ns($pkg) . "#$name";
 
     my $return_var = "g:vimx_symbiont_return['$name']";
 
-    # crutch
-    my $json = 1;
-
-    my $return_viml = $json
-        ? "let $return_var = json_decode('\$VIMx::Symbiont::RETURN{q{$name}}')"
-        : "let $return_var = '\$VIMx::Symbiont::RETURN{q{$name}}'"
-        ;
-
     my $viml = <<"END";
 function! $name(...) abort
-    perl ( \$VIMx::Symbiont::RETURN{q{$name}} = JSON::Tiny::encode_json( ${perl_name}(\@{ JSON::Tiny::decode_json(scalar VIM::Eval('json_encode(a:000)')) }) )) =~ s/'/''/g; VIM::DoCommand("$return_viml")
-    return $return_var
+    perl ${perl_name}(scalar VIM::Eval('json_encode(a:000)'))
+    return json_decode($return_var)
 endfunction
 END
+
+    my $wrapped = sub {
+        # vivify our args, execute the coderef, etc
+        my @a000 = @{ decode_json(scalar VIM::Eval('json_encode(a:000)')) };
+        ( $RETURN{$name} = encode_json($coderef->(@a000)) ) =~ s/'/''/g;
+
+        # handle getting the return value(s) back into vim-land
+        VIM::DoCommand("let $return_var = '$RETURN{$name}'");
+    };
 
     warn $viml;
     $VIML{$pkg} //= q{};
@@ -66,7 +66,7 @@ END
 
     {
         no strict 'refs';
-        *{$perl_name} = $coderef;
+        *{$perl_name} = $wrapped;
     }
 
     return;
