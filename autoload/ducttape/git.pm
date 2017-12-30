4c564319ecbbe277b000ac01338f77e0f549d4f2
Chris Weyl <cweyl@alumni.drew.edu>
2017-12-30 17:12:14 -0600

    wip! more git things

--- autoload/ducttape/git.pm
+++ autoload/ducttape/git.pm
@@ -41,43 +41,26 @@ function path_is_ignored  => sub { bufrepo->path_is_ignored(@_) };
 function merge_base       => sub { bufrepo->merge_base(@_)      };
 function status           => sub { bufrepo->status              };
 function revparse         => sub { [ bufrepo->revparse(@_) ]    };
-function revparse_count   => sub { scalar bufrepo->revparse(@_) };
+
+function revlist       => sub { [ map { $_->id } bufrepo->walker->push_range(bufrepo->revparse(@_))->all ] };
+function revlist_count => sub { scalar bufrepo->walker->push_range(bufrepo->revparse(@_))->all             };
+
+# function revparse_count   => sub { scalar bufrepo->revparse(@_) };
+# function revlist_count   => sub { scalar bufrepo->revparse(@_) };
 
 function fixup => sub {
-    my ($fn) = @_;
+    my ($id_to_fixup) = @_;
 
     # FIXME symlinks!
     # my $repo = Git::Raw::Repository->discover(path($main::curbuf->Name)->absolute->parent);
-    my $repo = Git::Raw::Repository->open($B{git_dir});
-    ### git dir: $B{git_commondir}
-    # my $repo = Git::Raw::Repository->open($B{git_commondir});
+    # my $repo = Git::Raw::Repository->open($b{git_dir});
+    my $repo = bufrepo;
 
     ### get our index and its corresponding tree...
     my $index = $repo->index;
-    ### count: $index->entry_count
-    ### version: $index->version
-    ### index path: $index->path
-    ### index checksum: $index->checksum
     $index->read(1);
-    ### count: $index->entry_count
-    ### index path: $index->path
-    ### index checksum: $index->checksum
     my $tree_id = $index->write_tree($repo);
     my $tree = $repo->lookup($tree_id);
-    # return;
-    # return;
-
-    ### $tree_id
-    ### tree is: $tree->id
-    ### tree: ref $tree
-    # return $tree->id;
-
-    # return;
-
-    ### owning path: $tree->owner->path
-    say $_->name for $tree->entries;
-
-    # return;
 
     ### get head for the fixup message...
     my $head = $repo->head;
@@ -85,16 +68,8 @@ function fixup => sub {
     ### head: $head_commit->id
     my $summary = $head_commit->summary;
 
-    # return;
-
     ### commit the fixup...
     my $who = Git::Raw::Signature->default($repo);
-    ### name: $who->name
-    ### email: $who->email
-    ### time: $who->time
-    ### $summary
-    # return;
-    # sleep 5;
     # my $fixup = $repo->commit(
     my $fixup = Git::Raw::Commit->create($repo,
         "fixup! $summary",
@@ -105,11 +80,9 @@ function fixup => sub {
         $tree,
         # 'refs/heads/test'
     );
-    # sleep 5;
 
-    return;
     ### done: $fixup->id
-    # return $fixup->id;
+    return $fixup->id;
 };
 
 function cat_file => sub {
@@ -117,9 +90,10 @@ function cat_file => sub {
 
     # FIXME symlinks!
     # my $repo = Git::Raw::Repository->discover(path($main::curbuf->Name)->absolute->parent);
-    # my $repo = Git::Raw::Repository->open($B{git_dir});
-    ### git dir: $B{git_commondir}
-    my $repo = Git::Raw::Repository->open($B{git_commondir});
+    # my $repo = Git::Raw::Repository->open($b{git_dir});
+    ### git dir: $b{git_commondir}
+    # my $repo = Git::Raw::Repository->open($b{git_commondir});
+    my $repo = bufrepo;
 
     my $contents = join("\n", $main::curbuf->Get(1..$main::curbuf->Count));
 
