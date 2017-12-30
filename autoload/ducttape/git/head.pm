1a8982416df9c94650faf7379f21cad9d4cbb6c6
Chris Weyl <cweyl@alumni.drew.edu>
2017-12-30 12:49:46 -0600

    Initial functions for manipulating HEAD

---
+++ autoload/ducttape/git/head.pm
@@ -0,0 +1,27 @@
+package ducttape::git::head;
+
+use v5.10;
+use strict;
+use warnings;
+
+use VIMx::Symbiont;
+use Git::Raw;
+use Git::Raw::Blob;
+use Git::Raw::Commit;
+use Git::Raw::Config;
+use Git::Raw::Repository;
+use Git::Raw::Signature;
+use Path::Tiny;
+
+# use ducttape::git;
+
+# debugging...
+use Smart::Comments '###';
+
+sub bufrepo { goto \&ducttape::git::bufrepo }
+
+function subject        => sub { bufrepo->head->peel('commit')->summary };
+function target_subject => sub { bufrepo->head->target->summary         };
+
+!!42;
+__END__
---
+++ autoload/ducttape/git/head.vim
@@ -0,0 +1,10 @@
+" For those following along at home:  the reason we can get away with
+" autoloaded functions is that we execute the VimL that creates them from
+" inside this script/file.  That's enough to convince vim that these functions
+" belong in that namespace -- or perhaps just that we're determined so it may
+" as well get out of the way.
+
+call ducttape#git#load() " ensure it's loaded
+execute ducttape#symbiont#autoload(expand('<sfile>'))
+
+" __END__
