6479ad4615161adcaf95ad9d763b1692daee485d
Chris Weyl <cweyl@alumni.drew.edu>
2018-01-20 22:24:59 -0600

    initial add: `ducttape::git::odb`

---
+++ autoload/ducttape/git/odb.pm
@@ -0,0 +1,37 @@
+package ducttape::git::odb;
+
+use v5.10;
+use strict;
+use warnings;
+
+use VIMx::Symbiont;
+use Git::Raw;
+use Git::Raw::Repository;
+
+use Smart::Comments;
+
+sub bufrepo {
+    # FIXME ARRGH
+    return Git::Raw::Repository->open($b{git_dir});
+
+    return unless $b{git_dir};
+    my $start = path(shift // $main::curbuf->Name || q{.})->absolute->parent;
+    return Git::Raw::Repository->discover($start);
+}
+
+fun args => 'id', read => sub {
+
+    my $obj = bufrepo->odb->read($a{id});
+
+    die "$a{id} was not found in the object database!"
+        unless !!$obj;
+
+    return {
+        map { $_ => $obj->$_() } qw{ id type size data },
+    };
+};
+
+
+
+!!42;
+__END__
---
+++ autoload/ducttape/git/odb.vim
@@ -0,0 +1,15 @@
+" For those following along at home:  the reason we can get away with
+" autoloaded functions is that we execute the VimL that creates them from
+" inside this script/file.  That's enough to convince vim that these functions
+" belong in that namespace -- or perhaps just that we're determined so it may
+" as well get out of the way.
+
+if !ducttape#require('Git::Raw')
+    " TODO FIXME create a FuncUndefined au here to warn?
+    let g:ducttape#git#odb#loaded = 0
+    finish
+endif
+
+execute ducttape#symbiont#autoload(expand('<sfile>'))
+
+" __END__
