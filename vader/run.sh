2050f9c46568e5ef6ba72b252a20041cd919ced4
Chris Weyl <cweyl@alumni.drew.edu>
2018-02-04 21:01:46 -0600

    Create a `vader/run.sh` helper
    ...why should travis get all the fun?

---
+++ vader/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+[ -d vader.vim ] || git clone --depth=1 --branch=spew-messages https://github.com/rsrchboy/vader.vim.git
+
+# sanity run of HTTP::Tiny w/included demo script
+# perl p5/http-tiny/eg/get.pl https://api.github.com/rate_limit
+
+_LIB=$(find p5/ -maxdepth 2 -name lib -type d -printf ':%p')
+find autoload -name '*.pm' | PERL5LIB=$PERL5LIB:$_LIB xargs -L1 perl -I autoload -I lib -I t/lib -MVIM -wc 2>&1
+
+vim -Nu vader/vimrc -c 'Vader! vader/*' 2>&1
