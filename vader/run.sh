963792d02a099d55310f6c9600b48f733c6fa8f0
Chris Weyl <cweyl@alumni.drew.edu>
2018-02-06 13:25:40 -0600

    `vim` loads a file when tests run
    So, this *must* happen for the tests to all pass.  `vim -c` is supposed
    to run "after the first file is loaded", and it appears that the `perl`
    interface isn't fully initialized until this happens, so anything
    involving accessing a buffer through `VIM::Buffers()` by _name_ will
    fail.

--- vader/run.sh
+++ vader/run.sh
@@ -8,4 +8,5 @@
 _LIB=$(find p5/ -maxdepth 2 -name lib -type d -printf ':%p')
 find autoload -name '*.pm' | PERL5LIB=$PERL5LIB:$_LIB xargs -L1 perl -I autoload -I lib -I t/lib -MVIM -wc 2>&1
 
-vim -Nu vader/vimrc -c 'Vader! vader/*' 2>&1
+# vim *MUST* also load a file here for all tests to pass
+vim -Nu vader/vimrc -c 'Vader! vader/*' README.md 2>&1
