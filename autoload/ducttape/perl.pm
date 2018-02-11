2ec7a39e2052cec4399c31b41b2ea74b092db850
Chris Weyl <cweyl@alumni.drew.edu>
2018-02-11 12:09:44 -0600

    Add `ducttape#perl#config()`
    Easy access to `%Config`.

--- autoload/ducttape/perl.pm
+++ autoload/ducttape/perl.pm
@@ -6,6 +6,8 @@ use v5.10;
 use strict;
 use warnings;
 
+use Config;
+
 use VIMx::Symbiont;
 
 # the VIMx::Symbiont-generated sub functions handles turning the parameters
@@ -19,5 +21,23 @@ function args => 'ver', version_ge => sub { $^V ge $a{ver} ? 1 : 0 };
 function args => 'ver', version_lt => sub { $^V lt $a{ver} ? 1 : 0 };
 function args => 'ver', version_le => sub { $^V le $a{ver} ? 1 : 0 };
 
+function args => 'key', config => sub { $Config{$a{key}} };
+
 !!42;
 __END__
+
+=head1 FUNCTIONS
+
+=head2 version()
+
+Returns the version of the linked Perl (C<$^V>), e.g. C<v5.26.1>.
+
+=head2 decimal_version()
+
+As with L</version()>, but in decimal form (C<$]>), e.g. c<5.026001>.
+
+=head2 config($key)
+
+Access to the L<Config> hash of Perl configuration values.
+
+=cut
